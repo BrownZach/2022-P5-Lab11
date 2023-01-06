a4c88704b93f5b0f0938620a4c1f5477a8f78add
Zach <zachary.brown@vtsd.com>
2023-01-06 11:01:47 -0500

    80%

--- Lab11avst.java
+++ Lab11avst.java
@@ -1,38 +1,41 @@
 // Lab11avst.java
 // The "Sieve of Eratosthenes" Program
 // This is the student, starting version of the Lab11a assignment.
-
+//ignore decimal format for 100%
+import org.w3c.dom.css.Counter;
 
 import java.util.Scanner;
-
-
-public class Lab11avst
-{
-    public static void main(String[] args)
-    {
+public class Lab11avst {
+    public static void main(String[] args) {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
+        System.out.println("Enter the primes upper bound ===>>");
+        final int MAX = input.nextInt();
         boolean primes[];
         primes = new boolean[MAX];
         computePrimes(primes);
         displayPrimes(primes);
     }
-
-    public static void computePrimes(boolean primes[])
-    {
+    public static void computePrimes(boolean primes[]) {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
-
+        for (int x = 2; x < primes.length; x++) {
+            primes[x] = true;
+        }
+        for (int x = 2; x < primes.length; x++) {
+            if(primes[x] == true){
+                for (int y = 2; y * x < primes.length; y++) {
+                    primes[(x * y)] = false;
+                }
+            }
+        }
     }
-
-    public static void displayPrimes(boolean primes[])
-    {
+    public static void displayPrimes(boolean primes[]) {
         System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
-
+        for(int x = 2; x < primes.length; x++) {
+            if (primes[x] == true) {
+                System.out.print(x + " ");
+            }
+        }
     }
-
 }
-
-
-
